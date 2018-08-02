b07ed3ca14f0f8cf04fc81e01c4a32cca95d58e0
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-02 10:15:45 -0300

    patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 mv numpy/distutils/environment.py numpy/distutils/fcompiler/environment.py
+# Let cython re-genatare this file.
+rm -f numpy/random/mtrand/mtrand.c
+rm -f PKG-INFO
 
 cat > site.cfg <<EOF
 [DEFAULT]
