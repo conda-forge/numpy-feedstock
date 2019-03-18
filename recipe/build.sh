83d5e5ce348ac3fd89515a54d0fef1d8125af4fd
Isuru Fernando <isuruf@gmail.com>
2019-03-17 23:24:12 -0500

    Simplify build.sh and fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-mv numpy/distutils/environment.py numpy/distutils/fcompiler/environment.py
-
-# Let cython re-genatare this file.
+# Let cython re-generate this file.
 rm -f numpy/random/mtrand/mtrand.c
 rm -f PKG-INFO
 
@@ -10,19 +8,6 @@ cat > site.cfg <<EOF
 [DEFAULT]
 library_dirs = $PREFIX/lib
 include_dirs = $PREFIX/include
-
-[atlas]
-atlas_libs = openblas
-libraries = openblas
-
-[openblas]
-libraries = openblas
-library_dirs = $PREFIX/lib
-include_dirs = $PREFIX/include
-
 EOF
 
-
-$PYTHON setup.py config
-$PYTHON setup.py build
-$PYTHON setup.py install
+$PYTHON -m pip install --no-deps --ignore-installed -v .
