aaac1b354b5390957fe4f4cf4766d3579da73b56
Isuru Fernando <isuruf@gmail.com>
2019-03-18 02:51:00 -0500

    Fix linking for osx and win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,7 @@
 echo [DEFAULT]
 echo library_dirs = %LIBRARY_LIB%
 echo include_dirs = %LIBRARY_INC%
+echo libraries = blas,cblas,lapack
 ) > site.cfg
 
 python -m pip install --no-deps --ignore-installed -v .
