68bddd151d059c0775bee9a1de061c9b0fe70413
Isuru Fernando <isuruf@gmail.com>
2019-03-18 00:04:22 -0500

    Blas 2.0 migration

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,10 @@
-%PYTHON% -m pip install --no-deps --ignore-installed .
+(
+echo [DEFAULT]
+echo library_dirs = %LIBRARY_LIB%
+echo include_dirs = %LIBRARY_INC%
+) > site.cfg
+
+python -m pip install --no-deps --ignore-installed -v .
 if errorlevel 1 exit 1
 
 XCOPY %RECIPE_DIR%\f2py.bat %SCRIPTS% /s /e
