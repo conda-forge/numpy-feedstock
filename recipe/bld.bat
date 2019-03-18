4113330e1f2815f7a979c9d365f11fa7d85d8829
Isuru Fernando <isuruf@gmail.com>
2019-03-18 00:13:49 -0500

    Fix f2py

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,3 +6,9 @@ echo include_dirs = %LIBRARY_INC%
 
 python -m pip install --no-deps --ignore-installed -v .
 if errorlevel 1 exit 1
+
+XCOPY %RECIPE_DIR%\f2py.bat %SCRIPTS% /s /e
+if errorlevel 1 exit 1
+
+del %SCRIPTS%\f2py.exe
+if errorlevel 1 exit 1
