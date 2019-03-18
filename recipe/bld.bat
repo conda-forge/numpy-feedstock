6643d3a8b063d4a2137bcca01f25b31535057ffb
Isuru Fernando <isuruf@gmail.com>
2019-03-17 23:57:35 -0500

    blas 2.0 migration

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,16 +2,7 @@
 echo [DEFAULT]
 echo library_dirs = %LIBRARY_LIB%
 echo include_dirs = %LIBRARY_INC%
-echo.
-echo [atlas]
-echo atlas_libs = openblas
-echo libraries = openblas
-echo.
-echo [openblas]
-echo libraries = openblas
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
 ) > site.cfg
 
-python setup.py build install --single-version-externally-managed --record=record.txt
+python -m pip install --no-deps --ignore-installed -v .
 if errorlevel 1 exit 1
