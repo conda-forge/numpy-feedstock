9fdbd163864035f13f6521696a3650dbd8e1b75d
Mark Harfouche <mark.harfouche@gmail.com>
2019-04-07 22:46:12 -0400

    undo other cflags changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ include_dirs = $PREFIX/include
 EOF
 
 # CFLAGS=-ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O3 -pipe -I$PREFIX/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix
-CFLAGS="-I$PREFIX/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix -O2"
+CFLAGS="$CFLAGS -O2"
 CPPFLAGS="$CPPFLAGS -O2"
 $PYTHON -m pip install --no-deps --ignore-installed -v .
 
