323eb147962bd198c43c4459fe86f04d08f6d91a
Isuru Fernando <isuruf@gmail.com>
2019-04-07 21:15:59 -0500

    Try -O0

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,8 @@ include_dirs = $PREFIX/include
 EOF
 
 # CFLAGS=-ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O3 -pipe -I$PREFIX/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix
-CFLAGS="-I$PREFIX/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
-
+CFLAGS="-I$PREFIX/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix -O0"
+CPPFLAGS="$CPPFLAGS -O0"
 $PYTHON -m pip install --no-deps --ignore-installed -v .
 
 set +ex
