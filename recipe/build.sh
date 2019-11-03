5779e51da2602a95cc8d7f3baea35060eb1a1351
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-11-03 10:34:40 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,4 +15,9 @@ if [[ "${target_platform}" == "linux-aarch64" ]]; then
     export CXX="$BUILD_PREFIX/bin/clang++"
 fi
 
+if [[ "${target_platform}" == "osx-64" ]] && [[ "${py}" -gte 38 ]]; then
+    export CFLAGS="${CFLAGS} -fno-lto"
+    export CXXFLAGS="${CXXFLAGS} -fno-lto"    
+fi
+
 $PYTHON -m pip install --no-deps --ignore-installed -v .
