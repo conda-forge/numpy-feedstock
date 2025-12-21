d2148f11bccce646509047d50b575f7165913077
H. Vetinari <h.vetinari@gmx.com>
2025-12-22 08:53:34 +1100

    set longdouble_format explicitly for all cross-compiled platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,15 +6,23 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
 mkdir builddir
 
-if [[ $host_platform == "osx-arm64" ]]; then
-    # currently cannot properly detect long double format
-    # on osx-arm64 when cross-compiling, see
+if [[ $build_platform != $host_platform ]]; then
+    # cannot properly detect long double format when cross-compiling, see
     # https://github.com/numpy/numpy/pull/24414
     # write to separate cross-file to not interfere with default cross-python activation, c.f.
     # https://github.com/conda-forge/cross-python-feedstock/blob/91d3c9cf/recipe/activate-cross-python.sh#L111-L125
-    echo "[properties]"                              > $SRC_DIR/osx-arm64_cross_file.txt
-    echo "longdouble_format = 'IEEE_DOUBLE_LE'"     >> $SRC_DIR/osx-arm64_cross_file.txt
-    export MESON_ARGS="$MESON_ARGS --cross-file=$SRC_DIR/osx-arm64_cross_file.txt"
+    echo "[properties]"                                      > $SRC_DIR/numpy_cross_file.txt
+    if [[ $host_platform == "linux-aarch64" ]]; then
+        echo "longdouble_format = 'IEEE_QUAD_LE'"           >> $SRC_DIR/numpy_cross_file.txt
+    elif [[ $host_platform == "linux-ppc64le" ]]; then
+        echo "longdouble_format = 'IBM_DOUBLE_DOUBLE_LE'"   >> $SRC_DIR/numpy_cross_file.txt
+    elif [[ $host_platform == "osx-arm64" ]]; then
+        echo "longdouble_format = 'IEEE_DOUBLE_LE'"         >> $SRC_DIR/numpy_cross_file.txt
+    fi
+    export MESON_ARGS="$MESON_ARGS --cross-file=$SRC_DIR/numpy_cross_file.txt"
+fi
+
+if [[ $host_platform == "osx-arm64" ]]; then
     # see #370 and https://github.com/numpy/numpy/issues/29820
     export CFLAGS="$CFLAGS -DACCELERATE_NEW_LAPACK"
     export CXXFLAGS="$CXXFLAGS -DACCELERATE_NEW_LAPACK"
